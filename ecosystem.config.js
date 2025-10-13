f2298356b494a9c430d33967dddd1f14dcd57a30
taysim <taysim@taysim.local>
2025-10-13 05:21:15 +0200

    fix: update ecosystem config and workflow for static files

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,14 @@
 module.exports = {
   apps: [{
-    name: 'cleanoo-app',
-    script: 'server.js',
+    name: 'cleanoo',
+    script: './.next/standalone/server.js',
+    cwd: '/var/www/cleanoo/web',
     instances: 1,
     exec_mode: 'cluster',
+    env_file: '.next/standalone/.env.production',
     env: {
       NODE_ENV: 'production',
       PORT: 3000
-    },
-    error_file: './logs/err.log',
-    out_file: './logs/out.log',
-    log_file: './logs/combined.log',
-    time: true
+    }
   }]
 }
-
-
